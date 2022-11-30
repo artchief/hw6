cd287adbf0d849358a38ff7c49e45accaf12639c
Artyom_Khachaturyan <artchief@gmail.com>
2022-11-30 17:46:24 +0300

    всё работает

--- 1/Program.cs
+++ 1/Program.cs
@@ -5,3 +5,27 @@
 
 // 1, -7, 567, 89, 223-> 4
 
+Console.Clear();
+int totalNegativeNumbers = 0;
+bool b = true;
+while (b)
+{
+    Console.Write("Хотите ввести число? Если  ДА наберите  1: ");
+
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    if (n == 1)
+    {
+        Console.Write("Введите число: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        if (m > 0)
+        {
+            totalNegativeNumbers = totalNegativeNumbers + 1;
+        }
+    }
+    else
+    {
+        Console.Write($"Спасибо! Чисел больше 0 введено {totalNegativeNumbers} раз");
+        b = false;
+    }
+}
